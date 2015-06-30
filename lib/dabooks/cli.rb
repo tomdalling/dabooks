2f832c118fd799df8741cacf48d3b706b4cdc209
Tom Dalling <tom@tomdalling.com>
2015-06-30 14:14:25 +1000

    Lists subcommands when ARGV empty

--- lib/dabooks/cli.rb
+++ lib/dabooks/cli.rb
@@ -4,7 +4,14 @@ module Dabooks::CLI
   extend self
 
   def run(argv=ARGV)
-    subcommand_name = argv.shift
+    if argv.empty?
+      print_subcommands
+    else
+      run_subcommand(argv.shift, argv)
+    end
+  end
+
+  def run_subcommand(subcommand_name, args)
     subcommand = begin
       const_get("#{subcommand_name.to_s.capitalize}Command")
     rescue NameError
@@ -13,9 +20,22 @@ module Dabooks::CLI
     end
 
     opts = Trollop.with_standard_exception_handling(subcommand::OPTIONS) do
-      subcommand::OPTIONS.parse(argv)
+      subcommand::OPTIONS.parse(args)
     end
-    subcommand.new(opts, argv).run
+    subcommand.new(opts, args).run
+  end
+
+  def print_subcommands
+    puts 'Available subcommands:'
+    parent_module = Dabooks::CLI
+    parent_module.constants.each do |const_sym|
+      const = parent_module.const_get(const_sym)
+      if const.is_a?(Module) && const.name.end_with?('Command')
+        _, _, subcommand_module_name = const.name.rpartition('::')
+        subcommand = subcommand_module_name.gsub(/Command\Z/, '').downcase
+        puts "    #{subcommand}"
+      end
+    end
   end
 end
 
